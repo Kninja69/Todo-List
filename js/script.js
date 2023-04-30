3644d86d8ff66e61254f7ff940b7ba2808d26d8b
Kninja69 <rafaelchaveslv@gmail.com>
2023-04-30 07:13:24 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 const form = document.querySelector('.form-task');
 const taskInput = document.querySelector('#new-task');
 const taskList = document.querySelector('.task-list .container');
+const taskDescriptionInput = document.querySelector('#new-task-description');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
-  addTask(taskInput.value);
+  taskadd(taskInput.value, taskDescriptionInput.value);
   taskInput.value = '';
+  taskDescriptionInput.value = '';
 });
 
-function addTask(task) {
+function taskadd(task, description) {
   const taskItem = document.createElement('div');
   taskItem.classList.add('task-item');
+
+  if (!task || !description) {
+    return;
+  }
+
+  // Verifica se a tarefa já existe na lista, e retorna sem adicionar a tarefa caso já exista
+  const existingTasks = document.querySelectorAll('.task-item p');
+  for (let i = 0; i < existingTasks.length; i++) {
+    if (existingTasks[i].textContent === task) {
+      return;
+    }
+  }
 
   const taskCheckbox = document.createElement('input');
   taskCheckbox.setAttribute('type', 'checkbox');
@@ -21,10 +35,15 @@ function addTask(task) {
   taskText.textContent = task;
   taskItem.appendChild(taskText);
 
+  const taskDescription = document.createElement('p');
+  taskDescription.classList.add('task-description');
+  taskDescription.textContent = description;
+  taskItem.appendChild(taskDescription);
+
   const taskEditButton = document.createElement('button');
   taskEditButton.classList.add('button');
   taskEditButton.innerHTML = '<i class="material-symbols-outlined">edit</i>';
-  taskEditButton.addEventListener('click', function() {
+  taskEditButton.addEventListener('click', function () {
     editTask(taskItem);
   });
   taskItem.appendChild(taskEditButton);
@@ -32,7 +51,7 @@ function addTask(task) {
   const taskDeleteButton = document.createElement('button');
   taskDeleteButton.classList.add('button');
   taskDeleteButton.innerHTML = '<i class="material-symbols-outlined">delete</i>';
-  taskDeleteButton.addEventListener('click', function() {
+  taskDeleteButton.addEventListener('click', function () {
     deleteTask(taskItem);
   });
   taskItem.appendChild(taskDeleteButton);
@@ -40,20 +59,40 @@ function addTask(task) {
   taskList.appendChild(taskItem);
 }
 
+function clearFields() {
+  taskInput.value = '';
+  taskDescriptionInput.value = '';
+}
+
+function handleAddTaskClick() {
+  const task = taskInput.value;
+  const description = taskDescriptionInput.value;
+  taskadd(task, description);
+  clearFields();
+}
+
 function editTask(taskItem) {
   const taskText = taskItem.querySelector('p');
+  const taskDescription = taskItem.querySelector('span');
   const taskEditTextbox = document.createElement('input');
+
   taskEditTextbox.classList.add('form-task-input');
   taskEditTextbox.setAttribute('type', 'text');
   taskEditTextbox.value = taskText.textContent;
   taskItem.replaceChild(taskEditTextbox, taskText);
 
+  const descriptionEditTextbox = document.createElement('input');
+  descriptionEditTextbox.classList.add('form-task-input');
+  descriptionEditTextbox.setAttribute('type', 'text');
+  descriptionEditTextbox.value = taskDescription.textContent;
+  taskItem.replaceChild(descriptionEditTextbox, taskDescription);
+
   const taskEditButton = taskItem.querySelector('.button:first-of-type');
   taskEditButton.innerHTML = '<i class="material-symbols-outlined">save</i>';
-  taskEditButton.removeEventListener('click', function() {
+  taskEditButton.removeEventListener('click', function () {
     editTask(taskItem);
   });
-  taskEditButton.addEventListener('click', function() {
+  taskEditButton.addEventListener('click', function () {
     saveTask(taskItem, taskEditTextbox.value);
   });
 }
@@ -63,15 +102,22 @@ function saveTask(taskItem, newTaskText) {
   taskText.textContent = newTaskText;
   taskItem.replaceChild(taskText, taskItem.querySelector('.form-task-input'));
 
+  const taskDescription = document.createElement('p');
+  taskDescription.classList.add('task-description');
+  const descriptionEditTextbox = taskItem.querySelector('.form-task-input ~ .form-task-input');
+  taskDescription.textContent = descriptionEditTextbox.value;
+  taskItem.replaceChild(taskDescription, taskItem.querySelector('.form-task-description'));
+
   const taskEditButton = taskItem.querySelector('.button:first-of-type');
   taskEditButton.innerHTML = '<i class="material-symbols-outlined">edit</i>';
-  taskEditButton.removeEventListener('click', function() {
+  taskEditButton.removeEventListener('click', function () {
     saveTask(taskItem, newTaskText);
   });
-  taskEditButton.addEventListener('click', function() {
+  taskEditButton.addEventListener('click', function () {
     editTask(taskItem);
   });
 }
+
 
 function deleteTask(taskItem) {
   taskItem.remove();
